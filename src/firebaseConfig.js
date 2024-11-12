ce7640f0222cde528df94d2b9d651db690b84dbf
akshathasalanke17@gmail.com <sakshatha.bsc23@rvu.edu.in>
2024-11-12 10:42:20 +0530

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuJb7zavhaErrN7_rHQCAuHoborlblBhI",
+  authDomain: "activity-07-f397a.firebaseapp.com",
+  databaseURL: "https://activity-07-f397a-default-rtdb.firebaseio.com",
+  projectId: "activity-07-f397a",
+  storageBucket: "activity-07-f397a.firebasestorage.app",
+  messagingSenderId: "619170246936",
+  appId: "1:619170246936:web:238d1287b50e3492b32e6d",
+  measurementId: "G-NY7MGY17P6"
+
+};
+
+const App = initializeApp(firebaseConfig);
+export default App;
